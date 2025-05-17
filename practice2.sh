de642786ee19d0d8f246330080b9d4dd68199d24
Ravikanti Sai <RavikantiS@emids.com>
2025-05-17 20:53:41 +0530

    practice 2

---
+++ practice2.sh
@@ -0,0 +1,10 @@
+ #!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+PERSON3=$3 
+
+ echo "$PERSON1 : $PERSON2 is my friend "
+ echo " $PERSON3 :i wanna play my game and run it"
+ echo "$PERSON1 $PERSON2 $PERSON3 :R.satyanarayana kiranam and genral "
+ echo "he is my friend"
