dd0551259db3de20754090a23b64f0e8e0279f50
Ravikanti Sai <RavikantiS@emids.com>
2025-05-19 21:13:31 +0530

    args

---
+++ install-command.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR: Please run the script with root access"
+    exit 1
+
+else
+    echo "You have root access"
+
+fi    
