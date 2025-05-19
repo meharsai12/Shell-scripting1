6320c9a9c2610c279bc60ff87baf6c030445955b
Ravikanti Sai <RavikantiS@emids.com>
2025-05-19 16:32:50 +0530

    args

--- arrays.sh
+++ arrays.sh
@@ -4,4 +4,5 @@
 
 MOVIES=("HIT" "LAMAN" "SALAAR")
 
-echo "First movie is :" $MOVIES[0]
+echo "First movie is :" ${MOVIES[0]} #we need to add flower brackets for output because the index 0 is not considering as variable 
+echo "second movie is :" ${MOVIES[1]}
