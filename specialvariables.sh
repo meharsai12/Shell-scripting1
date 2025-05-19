a9d598381fdd33e7a0f0a2b3f3249a7921a14fca
Ravikanti Sai <RavikantiS@emids.com>
2025-05-19 20:00:51 +0530

    args

--- specialvariables.sh
+++ specialvariables.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 echo "all variables passed to script : " $@
-echo "number of variables : " $#  
+echo "number of variables : " $#
+echo "script name : " $0
+echo "present working directory : " $PWD
+echo "user running this script : " $HOME
+echo " user running this script : " $USER  
+echo "pid of current script: " $$
+sleep 10 &
+echo "pid of command running background : " $! 
