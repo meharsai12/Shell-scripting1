10d853d7a365ae102affda7879371d7089a87072
Ravikanti Sai <RavikantiS@emids.com>
2025-05-19 16:35:38 +0530

    args

--- arrays.sh
+++ arrays.sh
@@ -5,4 +5,6 @@
 MOVIES=("HIT" "LAMAN" "SALAAR")
 
 echo "First movie is :" ${MOVIES[0]} #we need to add flower brackets for output because the index 0 is not considering as variable 
-echo "second movie is :" ${MOVIES[1]}
+echo "second movie is :" ${MOVIES[1]} # aboove and this command for single single movie name output for single movie or any variable outputs
+
+echo "all movies :" ${MOVIES[@]}
