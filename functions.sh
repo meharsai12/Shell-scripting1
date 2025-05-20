abbf8dd4af721e935ec1bb9297dcd1d8d4986d7c
Ravikanti Sai <RavikantiS@emids.com>
2025-05-20 15:45:24 +0530

    args

--- functions.sh
+++ functions.sh
@@ -4,54 +4,49 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
-    echo "ERROR: Please run the script with root access"
-    exit 1
-
+    echo "ERROR:: Please run this script with root access"
+    exit 1 #give other than 0 upto 127
 else
-    echo "You have root access"
+    echo "You are running with root access"
+fi
 
-fi 
-
-#below validation code is a function 
+# validate functions takes input as exit status, what command they tried to install
 VALIDATE(){
-    if ($1 -eq 0)
+    if [ $1 -eq 0 ]
     then
-        echo "$2 installation is ...sucess"
+        echo "Installing $2 is ... SUCCESS"
     else
-        echo "$2 installation is ...failure"
+        echo "Installing $2 is ... FAILURE"
         exit 1
-
-    fi        
-  }
-    dnf list installed mysql
-    if ($? -ne 0)
-    then    
-        echo "Mysql is not installed going to install it "
-        dnf install mysql -y
-        VALIDATE $? "MySQL"
-    else
-        echo "mysql is already installed nothing to do "
-
     fi
+}
 
-    dnf list installed python-3 
-    if ($? -ne 0)
-    then    
-        echo "python-3 is not installed going to install it "
-        dnf install python3 -y
-        VALIDATE $? "PYTHON 3"
-    else
-        echo "python 3 is already installed nothing to do "
+dnf list installed mysql
+if [ $? -ne 0 ]
+then
+    echo "MySQL is not installed... going to install it"
+    dnf install mysql -y
+    VALIDATE $? "MySQL"
+else
+    echo "MySQL is already installed...Nothing to do"
+fi
 
-    fi
+dnf list installed python3
+if [ $? -ne 0 ]
+then
+    echo "python3 is not installed... going to install it"
+    dnf install python3 -y
+    VALIDATE $? "python3"
+else
+    echo "python3 is already installed...Nothing to do"
+fi
 
-    dnf list installed nginx
-    if ($? -ne 0)
-    then    
-        echo "nginx is not installed going to install it "
-        dnf install ngnix -y
-        VALIDATE $? "NGNIX"
-    else
-        echo "nginx is already installed nothing to do "
-
-    fi
+dnf list installed nginx
+if [ $? -ne 0 ]
+then
+    echo "nginx is not installed... going to install it"
+    dnf install nginx -y
+    VALIDATE $? "nginx"
+else
+    echo "nginx is already installed...Nothing to do"
+fi
