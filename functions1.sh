222c6e6dd8cb46dd5d2522ec810ff76b80dae721
Ravikanti Sai <RavikantiS@emids.com>
2025-05-21 18:09:25 +0530

    args

---
+++ functions1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+TIMESTAMP=$(date)
+echo "script executed at $TIMESTAMP"
+
+
+USERID=$(id -u)
+
+if [$? -ne 0]
+then
+    echo " you dont have root access"
+else
+    echo "you have root access"
+fi
