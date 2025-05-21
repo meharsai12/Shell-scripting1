0e8af688e13a42a5c19ec111278747dc8519b93b
Ravikanti Sai <RavikantiS@emids.com>
2025-05-21 16:33:08 +0530

    args

--- functions1.sh
+++ functions1.sh
@@ -3,7 +3,7 @@
 USERID=$(id -u)
 
 
-if [$USERID -ne 0 ]
+if [ $USERID -ne 0 ]
 then
     echo " Error you dont have root access to run"
     exit 1
@@ -12,3 +12,5 @@ else
     echo " you have root access to run the command "
 
 fi
+  echo "You are running with root access"
+i
