eb37db7e7b167afee97ac86852ad25cdc5e12024
Ravikanti Sai <RavikantiS@emids.com>
2025-05-21 16:43:20 +0530

    args

--- functions1.sh
+++ functions1.sh
@@ -15,3 +15,23 @@ else
 
 fi
   echo "You are running with root access"
+
+
+dnf installed list mysql -y
+
+if [ $? -ne 0 ]
+then
+    echo " Mysql is not installed in your server going to install it"
+    dnf install mysql -y
+    if [ $? -eq 0 ]
+    then 
+        echo "Mysql is  installation is success "
+    else
+        echo "Mysql is installation is failure "
+        exit 1
+    fi
+    
+else
+    echo " mysql is already installed "
+
+fi
