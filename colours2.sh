b3742307a3415e7bf55b0f7d081fd405153b4f97
Ravikanti Sai <RavikantiS@emids.com>
2025-05-22 14:06:46 +0530

    args

--- colours2.sh
+++ colours2.sh
@@ -7,8 +7,10 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+
 then
-    echo "$R ERROR::   Please run this script with root access $N"
+
+    echo -e  "$R ERROR::   Please run this script with root access $N"
     exit 1 #give other than 0 upto 127
 else
     echo "You are running with root access"
